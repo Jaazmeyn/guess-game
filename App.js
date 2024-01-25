fb187436db42419b701d92d733b610c84624f6b7
Jasmin Blanda <jasmin.blanda@gmail.com>
2024-01-25 03:16:52 +0100

    gradient background fullscreen

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, ImageBackground } from 'react-native';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StartGameScreen />
+    <View style={styles.rootScreen}>
+      <ImageBackground 
+          source={require('./assets/images/background.png')} 
+          resizeMode='cover'
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    // backgroundColor: '#ddb52f',
+    flex:1,
+    backgroundImage: 'linear-gradient(to bottom, #ff0000, #72063c)'
+  },
+  backgroundImage: {
+    opacity: 0.11
+  }
+});
